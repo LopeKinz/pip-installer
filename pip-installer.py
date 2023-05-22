dfc6d38fb75963a1026d73f44260871753e2d2fe
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-05-22 06:42:53 +0000

    update

--- pip-installer.py
+++ pip-installer.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 def check_package_availability(package):
     try:
@@ -34,10 +35,25 @@ def view_extensions():
 
 def update_packages():
     try:
-        subprocess.check_call(['pip', 'list', '--outdated', '--format=freeze', '|', 'grep', '-v', '^-e', '|', 'cut', '-d', '=' '-f 1', '|', 'xargs', '-n1', 'pip', 'install', '-U'])
+        # Update pip
+        subprocess.check_call(['pip', 'install', '--upgrade', 'pip'])
+        
+        # Update other packages
+        subprocess.check_call('pip list --outdated  | grep -v \'^-e\' | cut -d \'=\' -f 1 | xargs -n1 pip install -U', shell=True)
+        
         print("Packages updated successfully.")
     except subprocess.CalledProcessError:
         print("Error updating packages.")
+
+def display_stats(start_time, errors, num_updated_installed):
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    print("---------- Statistics ----------")
+    print(f"Time elapsed: {elapsed_time:.2f} seconds")
+    print(f"Errors encountered: {errors}")
+    print(f"Number of updated/installed packages: {num_updated_installed}")
+    print("---------------------------------")
 
 def main():
     while True:
@@ -57,6 +73,10 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
+            start_time = time.time()
+            errors = 0
+            num_updated_installed = 0
+
             while True:
                 print("Enter the package name to install (or type 'q' to quit):")
                 package_name = input()
@@ -67,22 +87,54 @@ def main():
                 if check_package_availability(package_name):
                     print(f"Package '{package_name}' already installed")
                 else:
-                    install_package(package_name)
+                    try:
+                        install_package(package_name)
+                        num_updated_installed += 1
+                    except subprocess.CalledProcessError:
+                        errors += 1
+            
+            display_stats(start_time, errors, num_updated_installed)
         
         elif choice == '2':
+            start_time = time.time()
+            errors = 0
+            num_updated_installed = 0
+
             print("Enter package names separated by commas (or type 'q' to quit):")
             package_list = input()
             
             if package_list.lower() == 'q':
                 continue
 
-            install_packages(package_list)
+            try:
+                install_packages(package_list)
+                num_updated_installed = len(package_list.split(','))
+            except subprocess.CalledProcessError:
+                errors += 1
+            
+            display_stats(start_time, errors, num_updated_installed)
         
         elif choice == '3':
+            start_time = time.time()
+            errors = 0
+            num_updated_installed = 0
+
             view_extensions()
+
+            display_stats(start_time, errors, num_updated_installed)
         
         elif choice == '4':
-            update_packages()
+            start_time = time.time()
+            errors = 0
+            num_updated_installed = 0
+
+            try:
+                update_packages()
+                num_updated_installed = 1
+            except subprocess.CalledProcessError:
+                errors += 1
+            
+            display_stats(start_time, errors, num_updated_installed)
         
         elif choice == '0':
             break
@@ -92,4 +144,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
